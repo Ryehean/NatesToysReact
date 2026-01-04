7750d07b6e1d47ddc6e770be2363cfb291fc7e2d
Ryehean Jones <ryehean@gmail.com>
2026-01-04 16:11:57 -0500

    End of Section 6

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,14 +1,11 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]     //https://localhost:5001/api/products
-    [ApiController]
-    public class ProductsController(StoreContext context) : ControllerBase
+    public class ProductsController(StoreContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
@@ -16,7 +13,7 @@ namespace API.Controllers
             return await context.Products.ToListAsync();
         }
 
-        [HttpGet("{id}")]       // api/products/id
+        [HttpGet("{id}")] // api/products/2
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             var product = await context.Products.FindAsync(id);
