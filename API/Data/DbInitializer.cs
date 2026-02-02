5fc73b9ed3732378e51f8a3fc9a1dde1a444de3f
Ryehean Jones <ryehean@gmail.com>
2026-02-01 19:53:07 -0500

    Pre Publish

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -7,7 +7,7 @@ namespace API.Data;
 
 public class DbInitializer
 {
-    public static void InitDb(WebApplication app)
+    public static async Task InitDb(WebApplication app)
     {
         using var scope = app.Services.CreateScope();
 
@@ -18,10 +18,10 @@ public class DbInitializer
         ?? throw new InvalidOperationException("Failed to retrieve user manager");
 
 
-        SeedData(context, userManager);
+        await SeedData(context, userManager);
     }
 
-    private static async void SeedData(StoreContext context, UserManager<User> userManager)
+    private static async Task SeedData(StoreContext context, UserManager<User> userManager)
     {
         context.Database.Migrate();
 
